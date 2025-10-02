be8397b49d32cc23d6355aa3cc2536a9397a4ddb
chithanht7cbltk <thisun34@gmail.com>
2025-10-03 00:03:10 +0700

    init 1 for ex1

---
+++ STM32Cube/Core/Inc/ex1.h
@@ -0,0 +1,13 @@
+/*
+ * ex1.h
+ *
+ *  Created on: Oct 3, 2025
+ *      Author: cthanh
+ */
+
+#ifndef INC_EX1_H_
+#define INC_EX1_H_
+
+
+
+#endif /* INC_EX1_H_ */
---
+++ STM32Cube/Core/Src/ex1.c
@@ -0,0 +1,8 @@
+/*
+ * ex1.c
+ *
+ *  Created on: Oct 3, 2025
+ *      Author: HP
+ */
+
+
