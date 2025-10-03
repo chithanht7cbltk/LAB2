2706b63ce6be3878c6f04c83f97dfd481c7b2127
chithanht7cbltk <thisun34@gmail.com>
2025-10-03 19:40:56 +0700

    init 1 for ex2

---
+++ STM32Cube/Core/Inc/ex2.h
@@ -0,0 +1,13 @@
+/*
+ * ex2.h
+ *
+ *  Created on: Oct 3, 2025
+ *      Author: cthanh
+ */
+
+#ifndef INC_EX2_H_
+#define INC_EX2_H_
+
+
+
+#endif /* INC_EX2_H_ */
---
+++ STM32Cube/Core/Src/ex2.c
@@ -0,0 +1,9 @@
+/*
+ * ex2.c
+ *
+ *  Created on: Oct 3, 2025
+ *      Author: cthanh
+ */
+
+#include "ex2.h"
+
