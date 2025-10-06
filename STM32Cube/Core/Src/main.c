767eb25b3454afb27a7340653f1559f218b3a7ef
chithanht7cbltk <thisun34@gmail.com>
2025-10-07 00:14:27 +0700

    init 1 for ex9

--- STM32Cube/Core/Src/main.c
+++ STM32Cube/Core/Src/main.c
@@ -23,7 +23,7 @@
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
 #include "software_timer.h"
-#include "ex8.h"
+#include "ex9.h"
 /* USER CODE END Includes */
 
 /* Private typedef -----------------------------------------------------------*/
